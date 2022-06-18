cc9526a9f04e39cf073d8f372c53aa7ccfe57139
Hanna Bacelar <hannabacelar@hotmail.com>
2022-06-17 21:11:49 -0300

    :lipstick: feat crieando endpoint get talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,23 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// começando projeto aqui 
+ // requisito 1 
+function getTalker() {
+  return fs.readFile('./talker.json', 'utf-8')
+  .then((conteudoarquivo) => JSON.parse(conteudoarquivo));
+}
+
+app.get('/talker', async (req, res) => {
+try {
+  const talker = await getTalker();
+
+  return res.status(200).json(talker);
+} catch (error) { 
+  return res.status(200).end();
+}
 });
 
 app.listen(PORT, () => {
